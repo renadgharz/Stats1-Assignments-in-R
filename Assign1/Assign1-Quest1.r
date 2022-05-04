7489c143d8252b6704b32b35f51a4458453f403e
Renad <renad.gharz@outlook.com>
2022-05-03 22:18:54 -0600

    Assign1 Q1

--- Assign1/Assign1-Quest1.r
+++ Assign1/Assign1-Quest1.r
@@ -1,4 +1,4 @@
-data <- read.csv('Child Safety Seat Survey (Win).csv', header = TRUE)
+data <- read.csv('./Assign1/Child Safety Seat Survey (Win).csv', header = TRUE)
 
 # 1.1 Contingency Table
 crosstab <- table(data$AgeGroup, data$RestType)# Converting data to contingency table
@@ -11,11 +11,18 @@ prop_table_margin <- addmargins(prop_table)# Adding margins (sums) to proportion
 # No calculations required
 
 # 1.3 Side-by-Side Bar Chart
-barplot(crosstab)
+library(ggplot2) # Loading required package 
+library(tidyverse) # Loading required package
 
+dat = tibble(B=c(1,117,450,16), F=c(52,483,98,0), R=c(181,49,0,0), S=c(0,3,325,627))
+dat = dat %>%  
+  gather(key=dat$dat, value=value)
+
+ggplot(data=dat, aes(x=1:4, y=value, fill=dat)) + 
+  geom_bar(stat='identity', position='dodge')
 
 # 1.4 Pie and Stacked Bar Charts
-
+crosstab_stackedbars <- barplot(crosstab)
 
 
 # 1.5 Summary of Data
