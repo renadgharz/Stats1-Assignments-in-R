b73e86f74764f37b36c01ff01fe089ae1fd8f17c
Renad <renad.gharz@outlook.com>
2022-05-04 12:49:30 -0600

    Assign1 Question 2.1

--- Assign1/Assign1-Quest1.r
+++ Assign1/Assign1-Quest1.r
@@ -1,29 +1,57 @@
 data <- read.csv('./Assign1/Child Safety Seat Survey (Win).csv', header = TRUE)
 
+#Loading required libraries
+library(RColorBrewer)
+
 # 1.1 Contingency Table
 crosstab <- table(data$AgeGroup, data$RestType)# Converting data to contingency table
-crosstab_margin <- addmargins(crosstab)# Adding margins (sums) to contingency table
+crosstab_margins <- addmargins(crosstab)# Adding margins (sums) to contingency table
 
 prop_table <- prop.table(crosstab)# Converting contingency table to proportion format
-prop_table_margin <- addmargins(prop_table)# Adding margins (sums) to proportion contingency table
+prop_table_margins <- addmargins(prop_table)# Adding margins (sums) to proportion contingency table
 
 # 1.2 Data Types
 # No calculations required
 
 # 1.3 Side-by-Side Bar Chart
-library(ggplot2) # Loading required package 
-library(tidyverse) # Loading required package
+crosstab_inverse <- table(data$RestType, data$AgeGroup)
+crosstab_stackedbars <- barplot(crosstab_inverse, 
+                                beside = TRUE,
+                                main = "Restaint Types by Age Groups",
+                                sub = "Clustered Bar Chart",
+                                xlab = "Age Groups",
+                                ylab = "Frequencies",
+                                col = brewer.pal(n = 4, name = "Accent"),
+                                legend.text = c("B","F","R","S"),
+                                args.legend = list(x = "topleft", 
+                                                   bty = "n", 
+                                                   inset=c(0.01, 0)))
 
-dat = tibble(B=c(1,117,450,16), F=c(52,483,98,0), R=c(181,49,0,0), S=c(0,3,325,627))
-dat = dat %>%  
-  gather(key=dat$dat, value=value)
+# 1.4 Stacked Bar Charts
+crosstab_stackedbars <- barplot(crosstab_inverse,
+                                main = "Restaint Types by Age Groups",
+                                sub = "Stacked Bar Chart",
+                                xlab = "Age Groups",
+                                ylab = "Frequencies",
+                                col = brewer.pal(n = 4, name = "Accent"),
+                                legend.text = c("B","F","R","S"),
+                                args.legend = list(x = "topleft", 
+                                                   bty = "n", 
+                                                   inset=c(0.01, 0)))
 
-ggplot(data=dat, aes(x=1:4, y=value, fill=dat)) + 
-  geom_bar(stat='identity', position='dodge')
+prop_table_inverse <- prop.table(crosstab_inverse) * 100
+crosstab_stackedbars <- barplot(prop_table_inverse,
+                                main = "Restaint Types by Age Groups (%)",
+                                sub = "Stacked Bar Chart",
+                                xlab = "Age Groups",
+                                ylab = "Percentages",
+                                col = brewer.pal(n = 4, name = "Accent"),
+                                legend.text = c("B","F","R","S"),
+                                args.legend = list(x = "topleft", 
+                                                   bty = "n", 
+                                                   inset=c(0.01, 0)))
 
-# 1.4 Pie and Stacked Bar Charts
-crosstab_stackedbars <- barplot(crosstab)
-
+pie(crosstab_inverse)
 
 # 1.5 Summary of Data
 # No calculations required
--- Assign1/Assign1-Quest2.r
+++ Assign1/Assign1-Quest2.r
@@ -1,1 +1,10 @@
+# 2.1 Contingency Tables
+cable_service_table <- data.frame(B = c(8.2, 10.1, 18.3), 
+                                  B_c = c(10.9, 8.8, 19.7), 
+                                  Sum = c(19.1, 18.9, 38), 
+                                  row.names = c("A", "A_c", "Sum"))
 
+library(dplyr)
+cable_service_table_prop <- cable_service_table
+cable_service_table_prop %>% mutate(across(where(is.numeric)) / 38)
+                                    
