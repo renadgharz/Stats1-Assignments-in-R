f382b1cd530cf159fa3353369c78623f7b80b3c4
Renad <renad.gharz@outlook.com>
2022-05-04 22:22:59 -0600

    Question 2

--- Assign1/Assign1-Quest2.r
+++ Assign1/Assign1-Quest2.r
@@ -5,15 +5,22 @@ cable_service_table <- data.frame(B = c(8.2, 10.1, 18.3),
                                   row.names = c("A", "A_c", "Sum"))
 
 library(dplyr)
-cable_service_table_prop <- cable_service_table
-cable_service_table_prop %>% mutate(across(where(is.numeric)) / 38)
+cable_service_table_prop <- data.frame(B = c(8.2, 10.1, 18.3), 
+                                       B_c = c(10.9, 8.8, 19.7), 
+                                       Sum = c(19.1, 18.9, 38), 
+                                       row.names = c("A", "A_c", "Sum")) %>% 
+  mutate(across(where(is.numeric)) / 38)
 
 # 2.2 Probability of Union
+Prob_A_u_B <- cable_service_table_prop[1,3] + cable_service_table_prop[3,1] - cable_service_table_prop[1,1]
 
 # 2.3 Probability of Intersection
+Prob_Ac_n_Bc <- cable_service_table_prop[2,2]
 
 # 2.4 Mutually Exclusive Events
+# No calculations are required
 
 # 2.5 Independence of Events
+cable_service_table_prop[1,1] == cable_service_table_prop[1,3] * cable_service_table_prop[3,1]
 
 # 2.6 Conditional Probability
