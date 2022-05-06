da204b82af01d475d630191080a9044666f8f8b4
Renad <renad.gharz@outlook.com>
2022-05-06 09:30:56 -0600

    Question 2.6

--- Assign1/Assign1-Quest2.r
+++ Assign1/Assign1-Quest2.r
@@ -9,7 +9,7 @@ cable_service_table_prop <- data.frame(B = c(8.2, 10.1, 18.3),
                                        B_c = c(10.9, 8.8, 19.7), 
                                        Sum = c(19.1, 18.9, 38), 
                                        row.names = c("A", "A_c", "Sum")) %>% 
-  mutate(across(where(is.numeric)) / 38)
+  mutate(across(where(is.numeric)) / cable_service_table[3,3])
 
 # 2.2 Probability of Union
 Prob_A_u_B <- cable_service_table_prop[1,3] + cable_service_table_prop[3,1] - cable_service_table_prop[1,1]
