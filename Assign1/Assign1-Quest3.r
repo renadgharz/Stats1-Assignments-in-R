b06fa7216a02abfd3d3a1b60f74807941322cd0f
Renad <renad.gharz@outlook.com>
2022-05-06 10:30:50 -0600

    Question 3

--- Assign1/Assign1-Quest3.r
+++ Assign1/Assign1-Quest3.r
@@ -23,8 +23,21 @@ frontier_flights_prop <- data.frame(OnTime = c(231,1613,128,107,67,2146),
   mutate(across(where(is.numeric)) / frontier_flights[6,3])
 
 # 3.1 Marginal Probabilities
+delayed_delta_flights <- delta_flights_prop[6,2]
+delayed_frontier_flights <- frontier_flights_prop[6,2]
 
 # 3.2 Conditional Probabilities
+p_delta_delayed_given_los_angeles <- delta_flights_prop[1,2] / delta_flights_prop[1,3]
+p_delta_delayed_given_phoenix <- delta_flights_prop[2,2] / delta_flights_prop[2,3]
+p_delta_delayed_given_san_diego <- delta_flights_prop[3,2] / delta_flights_prop[3,3]
+p_delta_delayed_given_san_francisco <- delta_flights_prop[4,2] / delta_flights_prop[4,3]
+p_delta_delayed_given_seattle <- delta_flights_prop[5,2] / delta_flights_prop[5,3]
+
+p_frontier_delayed_given_los_angeles <- frontier_flights_prop[1,2] / frontier_flights_prop[1,3]
+p_frontier_delayed_given_phoenix <- frontier_flights_prop[2,2] / frontier_flights_prop[2,3]
+p_frontier_delayed_given_san_diego <- frontier_flights_prop[3,2] / frontier_flights_prop[3,3]
+p_frontier_delayed_given_san_francisco <- frontier_flights_prop[4,2] / frontier_flights_prop[4,3]
+p_frontier_delayed_given_seattle <- frontier_flights_prop[5,2] / frontier_flights_prop[5,3]
 
 # 3.3 Simpson's Paradox
 # No calculations required
