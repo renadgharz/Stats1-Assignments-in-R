a706b1caf6443a7c96321ecf40b92af31875492d
Renad <renad.gharz@outlook.com>
2022-05-06 09:44:26 -0600

    Question 2.6

--- Assign1/Assign1-Quest2.r
+++ Assign1/Assign1-Quest2.r
@@ -4,7 +4,6 @@ cable_service_table <- data.frame(B = c(8.2, 10.1, 18.3),
                                   Sum = c(19.1, 18.9, 38), 
                                   row.names = c("A", "A_c", "Sum"))
 
-library(dplyr)
 cable_service_table_prop <- data.frame(B = c(8.2, 10.1, 18.3), 
                                        B_c = c(10.9, 8.8, 19.7), 
                                        Sum = c(19.1, 18.9, 38), 
