e03ddb8483f56da6164c956ab6231fc80047e789
Renad <renad.gharz@outlook.com>
2022-05-08 08:37:13 -0600

    Code comments

--- Assign2/Assign2-Quest2.r
+++ Assign2/Assign2-Quest2.r
@@ -1,1 +1,5 @@
+# 2.1 Finding p and n
 
+# 2.2 Binomial Distribution
+
+# 2.3 Expected Value of Binomial Distribution
--- Assign2/Assign2-Quest3.r
+++ Assign2/Assign2-Quest3.r
@@ -1,1 +1,9 @@
+# 3.1 Probability of Single Event
 
+# 3.2 Poisson Distribution
+
+# 3.3 Coefficient of Variation for 1 House
+
+# 3.4 Expected Value of Poisson Distribution
+
+# 3.5 Coefficient of Variation for 100 Houses
