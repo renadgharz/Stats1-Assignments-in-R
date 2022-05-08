c728caeef107748fc346afaaecf1922a39781831
Renad <renad.gharz@outlook.com>
2022-05-08 14:32:57 -0600

    Headings

---
+++ Assign3/Assign3-Quest1.r
@@ -0,0 +1,7 @@
+# 1.1 Coefficient of Variation
+
+# 1.2 Confidence Interval for Costs
+
+# 1.3 Confidence Interval for Profits
+
+# 1.4 Coefficient of Variation of Profits
---
+++ Assign3/Assign3-Quest2.r
@@ -0,0 +1,15 @@
+# 2.1 Scatterplot
+
+# 2.2 Plot Analysis
+
+# 2.3 Extreme Points/Outliers
+
+# 2.4 Correlation Coefficient
+
+# 2.5 Negative Correlation Explanation
+
+# 2.6 Histogram - Wind Output
+
+# 2.7 Histogram - Solar Output
+
+# 2.8 Scaterrplot vs Histogram
